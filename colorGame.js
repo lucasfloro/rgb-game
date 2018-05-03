cf5b11ebe968c3ee0077b7fb50009592ce03cce6
Lucas Flóro <lucas.floro@gmail.com>
2018-05-02 22:39:38 -0300

    Add rgb color on h1 on html. Add pickedColor on js

--- colorGame.js
+++ colorGame.js
@@ -7,10 +7,28 @@ let colors = [
     "rgb(255, 0, 255)"
 ]
 
+let pickedColor = colors[3];
+let colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+
 let squares = document.querySelectorAll(".square");
 
 for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
+
+    squares[i].addEventListener("click", function() {
+        // grab color of clicked square
+        let clickedColor = this.style.backgroundColor;
+
+        //compare color to pickedColor
+        if (clickedColor === pickedColor) {
+            alert("you searched the color");
+        } else {
+            this.style.backgroundColor = "#232323";
+        }
+    })
 }
 // squares.forEach(element => {
 //     element.style
