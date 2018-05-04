76a37e5b7d8ab5b1244012bfbfb6403bd7738c7e
Lucas Flóro <lucas.floro@gmail.com>
2018-05-03 22:31:37 -0300

    Add function to generate random colors automatically

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,10 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+let colors = generateRandomColors(6);
 
 let pickedColor = pickColor();
 let squares = document.querySelectorAll(".square");
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.getElementById("message");
+let h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,8 +17,9 @@ for (let i = 0; i < squares.length; i++) {
 
         //compare color to pickedColor
         if (clickedColor === pickedColor) {
-            messageDisplay.textContent = "CORRECT!";
+            messageDisplay.textContent = "CORRECT!";            
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Error! Try Again!";
@@ -42,4 +37,27 @@ function changeColors(color) {
 function pickColor() {
     let random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    // make an array
+    let arr = [];
+    
+    // add num random colors to array
+    for(let i = 0; i < num; i++) {
+        //get random color an push into arr
+        arr.push(randomColor());
+    }
+
+    // return array
+    return arr;
+}
+
+function randomColor() {
+    //pick a rgb color (red, green, blue) 
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
 }
